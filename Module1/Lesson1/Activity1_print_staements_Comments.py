1341982ade961a41a6659967e4fb23833a596f18
ZeeshanHawaldar <zeeshanhawaldar@gmail.com>
2025-12-25 18:49:58 +0530

    Initial commit

---
+++ Module1/Lesson1/Activity1_print_staements_Comments.py
@@ -0,0 +1,11 @@
+#printing a greeting message
+print("Hello, world!")
+
+#Printing my name
+print("My name is Zeeshan.")
+
+#printing fun fact about myself
+print("A fun fact about me is that I love PLaying Chess.")
+
+#Performing a simple arithmetic operation and printing the result
+print("The sum of 5 and 3 is:", 5 + 3)
