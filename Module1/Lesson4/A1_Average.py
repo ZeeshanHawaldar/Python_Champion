02902236d59fc176aa0b60f20cf2369797b4c79a
ZeeshanHawaldar <zeeshanhawaldar@gmail.com>
2025-12-29 19:37:03 +0530

    Added lesson4

---
+++ Module1/Lesson4/A1_Average.py
@@ -0,0 +1,13 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+print("finding total of trees")
+total=tree1+tree2+tree3+tree4+tree5
+print(total)
+
+print("finding average of trees")
+average=total/5 
+print(average)
---
+++ Module1/Lesson4/ACP_Squareroot.py
@@ -0,0 +1,4 @@
+#Square root of a number
+num = int(input("Enter a number: "))
+sqrt = int(num ** (1/2))
+print("The square root of", num, "is", sqrt)
